cc3a584b729af60cc85ca4fa8d81260074698826
Dawid Chmal <d.chmalu@gmail.com>
2024-06-10 16:00:10 +0200

    [10.06.2024] [0.1.1] -zmiana sposobu wysyłania maili z usług firm trzecich na google -użycie 'hasła do aplikacji' zamiast zwykłego logowania

--- src/halina/email_rapport/email_sender.py
+++ src/halina/email_rapport/email_sender.py
@@ -7,10 +7,11 @@ logger = logging.getLogger(__name__.rsplit('.')[-1])
 
 
 class EmailSender:
-    SENDGRID_API_KEY = 'API'
+    EMAIL_APP_PASSWORD = ''
     FROM_EMAIL = 'dchmal@akond.com'
-    SMTP_HOST = 'smtp.sendgrid.net'
+    SMTP_HOST = 'smtp.gmail.com'
     SMTP_PORT = 587
+
 
     def __init__(self, to_email, subject, content):
         self.to_email = to_email
@@ -28,18 +29,33 @@ class EmailSender:
 
         try:
             await smtp.connect()
-            await smtp.login("apikey", self.SENDGRID_API_KEY)
+            await smtp.login(self.FROM_EMAIL, self.EMAIL_APP_PASSWORD)
             await smtp.send_message(message)
             await smtp.quit()
             logger.info(f"Email sent successfully to {self.to_email}")
+            print("Udało się wysłać maila")
             return True
         except SMTPException as e:
             logger.error(f"Failed to send email due to SMTP error: {str(e)}")
+            print("Nie udało się wysłać maila")
             return False
         except asyncio.TimeoutError:
             logger.error("Failed to send email due to a timeout error")
+            print("Nie udało się wysłać maila")
             return False
         except Exception as e:
             logger.error(f"An unexpected error occurred: {str(e)}")
+            print("Nie udało się wysłać maila")
             return False
 
+async def main():
+    email_sender = EmailSender(
+        to_email="d.chmalu@gmail.com",
+        subject="Test Email",
+        content="Testowy mail do wysyłania raportów z obserwatorium"
+    )
+    result = await email_sender.send()
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
