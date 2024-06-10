0305976ee416c9c1e6d827641a8aa1590565f61c
Dawid Chmal <d.chmalu@gmail.com>
2024-06-10 16:10:51 +0200

    [10.06.2024] [0.1.1] -usunięcie zbędnych lini

--- src/halina/email_rapport/email_sender.py
+++ src/halina/email_rapport/email_sender.py
@@ -12,7 +12,6 @@ class EmailSender:
     SMTP_HOST = 'smtp.gmail.com'
     SMTP_PORT = 587
 
-
     def __init__(self, to_email, subject, content):
         self.to_email = to_email
         self.subject = subject
@@ -33,29 +32,13 @@ class EmailSender:
             await smtp.send_message(message)
             await smtp.quit()
             logger.info(f"Email sent successfully to {self.to_email}")
-            print("Udało się wysłać maila")
             return True
         except SMTPException as e:
             logger.error(f"Failed to send email due to SMTP error: {str(e)}")
-            print("Nie udało się wysłać maila")
             return False
         except asyncio.TimeoutError:
             logger.error("Failed to send email due to a timeout error")
-            print("Nie udało się wysłać maila")
             return False
         except Exception as e:
             logger.error(f"An unexpected error occurred: {str(e)}")
-            print("Nie udało się wysłać maila")
             return False
-
-async def main():
-    email_sender = EmailSender(
-        to_email="d.chmalu@gmail.com",
-        subject="Test Email",
-        content="Testowy mail do wysyłania raportów z obserwatorium"
-    )
-    result = await email_sender.send()
-    print(result)
-
-if __name__ == "__main__":
-    asyncio.run(main())
