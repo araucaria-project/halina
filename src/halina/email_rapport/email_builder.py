e665e8316d8a57f626a755e7818b60b601848fe1
Dawid Chmal <d.chmalu@gmail.com>
2024-06-11 13:32:54 +0200

    Email template tables with photo [11.06.2024] [0.1.0] - dodanie templatki do tworzenia maili z tabelą tekstem i zdjęciem

--- src/halina/email_rapport/email_builder.py
+++ src/halina/email_rapport/email_builder.py
@@ -1,20 +1,11 @@
 import logging
+from jinja2 import Environment, FileSystemLoader
+import os
 
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
 
-class EmailBuilder:
-    # todo zrobić tak jak w modelach w java
-    def __init__(self):
-        self._date = None
-
-    def set_date(self, date):
-        self._date = date
-
-    def date(self, date):
-        self._date = date
-        return self
-
-    async def build(self) -> str:
-        """this method return complete email as string"""
-        return ""
+def generate_email_content(template_name, context):
+    env = Environment(loader=FileSystemLoader(os.path.dirname(__file__)))
+    template = env.get_template(template_name)
+    return template.render(context)
---
+++ src/halina/test.py
@@ -0,0 +1,30 @@
+import asyncio
+from email_rapport.email_sender import EmailSender
+
+
+async def main():
+    context = {
+        'message': 'To jest przykładowy mail z tabelą i zdjęciem.',
+        'table_data': [
+            ['Kolumna 1', 'Kolumna 2'],
+            ['Wartość 1', 'Wartość 2'],
+            ['Wartość 3', 'Wartość 4']
+        ]
+    }
+
+    email_sender = EmailSender(
+        to_email='d.chmalu@gmail.com',
+        subject='Test Przykładowy mail z tabelą i zdjęciem',
+        template_name='email_template.html',
+        context=context,
+        image_path='src/halina/email_rapport/zdjecie.png',
+    )
+
+    result = await email_sender.send()
+    if result:
+        print("Mail wysłany pomyślnie!")
+    else:
+        print("Wysyłka maila nie powiodła się.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
