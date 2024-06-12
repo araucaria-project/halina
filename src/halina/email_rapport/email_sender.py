d217a82edb9f0f56319df6a58779fa970b31d2e8
Dawid Chmal <d.chmalu@gmail.com>
2024-06-12 11:01:20 +0200

    Fix z rodzieleniem sendera od gneratora[12.06.2024] [0.1.0]

--- src/halina/email_rapport/email_sender.py
+++ src/halina/email_rapport/email_sender.py
@@ -1,11 +1,7 @@
 import asyncio
 import logging
 from aiosmtplib import SMTP, SMTPException
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-from .email_builder import generate_email_content
 
 logger = logging.getLogger(__name__.rsplit('.')[-1])
 
@@ -16,32 +12,12 @@ class EmailSender:
     SMTP_HOST = 'smtp.gmail.com'
     SMTP_PORT = 587
 
-    def __init__(self, to_email, subject, template_name, context, image_path=None):
+    def __init__(self, to_email: str):
         self.to_email = to_email
-        self.subject = subject
-        self.template_name = template_name
-        self.context = context
-        self.image_path = image_path
 
-    async def send(self):
-        message = generate_email_content(
-            from_email=self.FROM_EMAIL,
-            to_email=self.to_email,
-            subject=self.subject,
-            template_name=self.template_name,
-            context=self.context,
-            image_path=self.image_path
-        )
-
-        smtp = SMTP(hostname=self.SMTP_HOST, port=self.SMTP_PORT, start_tls=True)
-
-        if self.image_path:
-            with open(self.image_path, 'rb') as img:
-                img_data = img.read()
-            image = MIMEImage(img_data)
-            image.add_header('Content-ID', '<image1>')
-            image.add_header('Content-Disposition', 'inline', filename='image1.png')
-            message.attach(image)
+    async def send(self, message: MIMEMultipart):
+        message["From"] = EmailSender.FROM_EMAIL
+        message["To"] = EmailSender.SMTP_HOST
 
         smtp = SMTP(hostname=self.SMTP_HOST, port=self.SMTP_PORT, start_tls=True)
 
@@ -53,10 +29,4 @@ class EmailSender:
                 return True
         except SMTPException as e:
             logger.error(f"Failed to send email due to SMTP error: {str(e)}")
-            return False
-        except asyncio.TimeoutError:
-            logger.error("Failed to send email due to a timeout error")
-            return False
-        except Exception as e:
-            logger.error(f"An unexpected error occurred: {str(e)}")
             return False
